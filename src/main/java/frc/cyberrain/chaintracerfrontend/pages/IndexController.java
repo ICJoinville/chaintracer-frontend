100a363aba788ce8dd314f6a6c578e8e8f26c8b8
João Rodrigo [Development Leadership] <joao_luckow@estudante.sesisenai.org.br>
2024-08-09 07:54:40 -0300

    fix: BlockRepository not private on IndexController.java

--- src/main/java/frc/cyberrain/chaintracerfrontend/pages/IndexController.java
+++ src/main/java/frc/cyberrain/chaintracerfrontend/pages/IndexController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
     @Autowired
-    BlockRepository blockRepository;
+    private BlockRepository blockRepository;
     @Autowired
     private TransactionRepository transactionRepository;
     @Autowired
